e367dc71c2270fb0d9b4de7666b699c117844433
aceail <38589098+aceail@users.noreply.github.com>
2025-07-08 12:26:47 +0900

    Improve JSON diff normalization

--- llm_review_project/editor/templatetags/diff_tags.py
+++ llm_review_project/editor/templatetags/diff_tags.py
@@ -1,13 +1,35 @@
 from django import template
 from django.utils.safestring import mark_safe
 import difflib
+import json
 
 register = template.Library()
 
 @register.simple_tag
 def diff_highlight(new_text, old_text='', color_class='text-red-600'):
-    """Return HTML diff highlighting additions with the given color."""
+    """Return HTML diff highlighting additions with the given color.
+
+    Leading/trailing or internal whitespace differences can occur when
+    converting JSON to strings. To avoid false highlights, compare the two
+    strings after removing all whitespace characters. If they match, return
+    the new text without any highlighting.
+    """
+
     if not old_text:
+        return mark_safe(new_text)
+
+    # Try JSON normalization first to avoid false positives caused by
+    # formatting differences or key ordering.
+    try:
+        new_obj = json.loads(new_text)
+        old_obj = json.loads(old_text)
+        new_normalized = json.dumps(new_obj, ensure_ascii=False, sort_keys=True)
+        old_normalized = json.dumps(old_obj, ensure_ascii=False, sort_keys=True)
+    except Exception:
+        new_normalized = ''.join(new_text.split())
+        old_normalized = ''.join(old_text.split())
+
+    if new_normalized == old_normalized:
         return mark_safe(new_text)
 
     diff = difflib.ndiff(old_text.split(), new_text.split())
