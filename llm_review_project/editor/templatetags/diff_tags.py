dd4368043f2ffb7305c5203977085fc517458790
aceail <38589098+aceail@users.noreply.github.com>
2025-07-08 11:22:57 +0900

    Show diff highlights with user colors

--- llm_review_project/editor/templatetags/diff_tags.py
+++ llm_review_project/editor/templatetags/diff_tags.py
@@ -4,20 +4,22 @@ import difflib
 
 register = template.Library()
 
-@register.filter
-def diff_highlight(new_text, old_text):
-    """Return HTML diff showing additions in <span> tags."""
+@register.simple_tag
+def diff_highlight(new_text, old_text='', color_class='text-red-600'):
+    """Return HTML diff highlighting additions with the given color."""
     if not old_text:
-        return new_text
+        return mark_safe(new_text)
+
     diff = difflib.ndiff(old_text.split(), new_text.split())
-    result = []
+    pieces = []
     for part in diff:
         code = part[:2]
         text = part[2:]
         if code == '+ ':
-            result.append(f'<span class="diff-added">{text}</span>')
+            pieces.append(f'<span class="{color_class} diff-added">{text}</span>')
         elif code == '- ':
             continue
         else:
-            result.append(text)
-    return mark_safe(' '.join(result))
+            pieces.append(text)
+
+    return mark_safe(' '.join(pieces))
