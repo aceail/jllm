356fcb4e7e806d95c5708d1014b281c89efab221
yjpark <yjpark@jlkgroup.com>
2025-07-08 00:13:47 +0000

    user 추가

--- llm_review_project/editor/admin.py
+++ llm_review_project/editor/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import InferenceResult, InferenceImage
 
 # Register your models here.
+admin.site.register(InferenceResult)
+admin.site.register(InferenceImage)
